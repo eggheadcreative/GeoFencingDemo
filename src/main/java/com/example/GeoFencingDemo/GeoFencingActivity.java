118e29e5fccab57e49c966f1617a27a57f9f744c
Prembabu Panchadara <prem@eggheadcreative.com>
2013-09-26 11:07:38 +0530

    Implemented Bugsnage, Google Analytics, and Multiple Environment functionalities

--- src/main/java/com/example/GeoFencingDemo/GeoFencingActivity.java
+++ src/main/java/com/example/GeoFencingDemo/GeoFencingActivity.java
@@ -2,6 +2,9 @@ package com.example.GeoFencingDemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.bugsnag.android.Bugsnag;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.GoogleAnalytics;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,5 +16,26 @@ import android.os.Bundle;
 public class GeoFencingActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Bugsnag.register(this, "7977d834798df3256fcfec6e847a6902");
+    Bugsnag.setNotifyReleaseStages("production", "staging", "demo");
+    Bugsnag.setReleaseStage(getResources().getString(R.string.environment_name));
   }
+
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(this);
+    googleAnalytics.getTracker(getResources().getString(R.string.ga_trackingId));
+    EasyTracker.getInstance().setContext(this);
+    EasyTracker.getInstance().activityStart(this);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    EasyTracker.getInstance().setContext(this);
+    EasyTracker.getInstance().activityStop(this);
+  }
+
 }
