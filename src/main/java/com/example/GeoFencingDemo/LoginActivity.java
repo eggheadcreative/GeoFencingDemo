e64ebc93035c62f0d92c5d1e81135135357efe0c
Prembabu Panchadara <prem@eggheadcreative.com>
2013-09-26 13:12:09 +0530

    implemented Basic functionality in LogIn page

--- src/main/java/com/example/GeoFencingDemo/LoginActivity.java
+++ src/main/java/com/example/GeoFencingDemo/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.GeoFencingDemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,9 +13,35 @@ import android.os.Bundle;
  * Time: 10:54 AM
  * To change this template use File | Settings | File Templates.
  */
-public class LoginActivity extends GeoFencingActivity {
+public class LoginActivity extends GeoFencingActivity implements View.OnClickListener {
+
+  private EditText memberId;
+  private Button signIn;
+
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.login);
+    getWidgets();
+  }
+
+  private void getWidgets() {
+    memberId = (EditText) findViewById(R.id.input_memberid);
+    signIn = (Button) findViewById(R.id.login);
+    signIn.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    String inputId = memberId.getText().toString();
+    if (inputId.equals("")) {
+      memberId.setError("Pls Enter Member Id");
+    } else {
+      startHomeIntent();
+    }
+  }
+
+  public void startHomeIntent() {
+    Intent homeIntent = new Intent(this, HomeActivity.class);
+    startActivity(homeIntent);
   }
 }
