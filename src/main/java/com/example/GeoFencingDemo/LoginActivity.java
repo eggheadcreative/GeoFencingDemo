1e371f9064ed534972ff07debe245b49fce8a35c
Prembabu Panchadara <prem@eggheadcreative.com>
2013-09-26 11:25:48 +0530

    Added App icon, and replaced default theam with holo loght

--- src/main/java/com/example/GeoFencingDemo/LoginActivity.java
+++ src/main/java/com/example/GeoFencingDemo/LoginActivity.java
@@ -13,5 +13,6 @@ import android.os.Bundle;
 public class LoginActivity extends GeoFencingActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.login);
   }
 }
