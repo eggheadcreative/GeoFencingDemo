b412e9a278bc454a3814c17d48903d50b0f9db87
Prembabu Panchadara <prem@eggheadcreative.com>
2013-10-03 16:43:14 +0530

    showing GoogleMap Grid with current Location

--- src/main/java/com/ehc/GeoFencingDemo/HomeActivity.java
+++ src/main/java/com/ehc/GeoFencingDemo/HomeActivity.java
@@ -1,31 +1,25 @@
 package com.ehc.GeoFencingDemo;
 
-import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.location.*;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-//import com.google.android.gms.maps.MapView;
-import com.google.android.maps.MapView;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.maps.MapActivity;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 import java.util.Locale;
@@ -35,13 +29,13 @@ public class HomeActivity extends GeoFencingActivity implements LocationListener
     GooglePlayServicesClient.OnConnectionFailedListener {
 
   private TextView existingDataView;
-  private ImageView mapImage;
   private Button startCapture;
   private LocationRequest mLocationRequest;
   public LocationClient mLocationClient;
-  private MapView mapView;
   private LinearLayout mapLayout;
+  private MapView mapView;
   private GoogleMap googleMap;
+  private LatLng currentLocation;
 
 
   @Override
@@ -50,7 +44,7 @@ public class HomeActivity extends GeoFencingActivity implements LocationListener
     setContentView(R.layout.home);
     checkGooglePlayService();
     getWidgets();
-//    mapView.onCreate(savedInstanceState);
+    showMap(savedInstanceState);
   }
 
   private void checkGooglePlayService() {
@@ -62,10 +56,6 @@ public class HomeActivity extends GeoFencingActivity implements LocationListener
     }
   }
 
-//  @Override
-//  protected boolean isRouteDisplayed() {
-//    return false;
-//  }
 
   @Override
   protected void onStart() {
@@ -84,26 +74,51 @@ public class HomeActivity extends GeoFencingActivity implements LocationListener
   private void findLocation() {
     Location currentLocation = mLocationClient.getLastLocation();
     mLocationRequest = LocationRequest.create();
-    (new HomeActivity.GetAddressTask(this)).execute(currentLocation);
+    setLocation(currentLocation);
   }
 
   private void getWidgets() {
     existingDataView = (TextView) findViewById(R.id.existing_data);
     existingDataView.setTypeface(Typeface.createFromAsset(getAssets(), "RobotoSlab-Regular.ttf"));
-    mapImage = (ImageView) findViewById(R.id.map_image);
     startCapture = (Button) findViewById(R.id.start_capture);
     startCapture.setOnClickListener(this);
     mapLayout = (LinearLayout) findViewById(R.id.map_layout);
-    mapView = new MapView(getBaseContext(), getResources().getString(R.string.api_debug_key));
-    mapView.setBuiltInZoomControls(true);
-    mapView.displayZoomControls(true);
-
-    mapView.setEnabled(true);
-//    googleMap = mapView.getMap();
-    mapLayout.addView(mapView);
     mLocationClient = new LocationClient(this, this, this);
-
   }
+
+  private void showMap(Bundle savedInstanceState) {
+    try {
+      MapsInitializer.initialize(getBaseContext());
+    } catch (GooglePlayServicesNotAvailableException e) {
+      e.printStackTrace();
+    }
+    GoogleMapOptions options = new GoogleMapOptions();
+    options.mapType(GoogleMap.MAP_TYPE_SATELLITE);
+    options.zoomControlsEnabled(true);
+    mapView = new MapView(this, options);
+    mapView.onCreate(savedInstanceState);
+    mapView.onResume();
+    mapView.setEnabled(true);
+    googleMap = mapView.getMap();
+    if (googleMap != null) {
+      googleMap.setMyLocationEnabled(true);
+    } else
+      Log.d("googlemap:", "null");
+    mapLayout.addView(mapView);
+  }
+
+  private void focusCurrentLocation(Address address) {
+    currentLocation = new LatLng(address.getLatitude(), address.getLongitude());
+    googleMap.addMarker(new MarkerOptions()
+        .position(currentLocation)
+        .title("location")
+        .snippet("location is cool")
+        .icon(BitmapDescriptorFactory
+            .fromResource(R.drawable.ic_launcher)));
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+    googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+  }
+
 
   @Override
   public void onClick(View view) {
@@ -132,56 +147,43 @@ public class HomeActivity extends GeoFencingActivity implements LocationListener
 
   }
 
-  protected class GetAddressTask extends AsyncTask<Location, Void, String> {
-
-    Context localContext;
-
-    public GetAddressTask(Context context) {
-      super();
-      localContext = context;
+  protected void setLocation(Location params) {
+    Geocoder geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+    String locationDetails = "";
+    Location location = params;
+    List<Address> addresses = null;
+    try {
+      addresses = geocoder.getFromLocation(location.getLatitude(),
+          location.getLongitude(), 1);
+    } catch (Exception exception) {
+      exception.printStackTrace();
     }
 
-    @Override
-    protected String doInBackground(Location... params) {
-      Geocoder geocoder = new Geocoder(localContext, Locale.getDefault());
-      String locationDetails = "";
-      Location location = params[0];
-      List<Address> addresses = null;
-      try {
-        addresses = geocoder.getFromLocation(location.getLatitude(),
-            location.getLongitude(), 1);
-      } catch (Exception exception) {
-        exception.printStackTrace();
-      }
-
-      if (addresses != null && addresses.size() > 0) {
-        Address address = addresses.get(0);
-        locationDetails = getLocationDetails(address);
-      }
-      return locationDetails;
+    if (addresses != null && addresses.size() > 0) {
+      Address address = addresses.get(0);
+      locationDetails = getLocationDetails(address);
+      focusCurrentLocation(address);
     }
-
-    public String getLocationDetails(Address address) {
-//todo will remove unnecessary info
-      return
-          "\nAddress Line       : " + address.getAddressLine(0) +
-              "\nSubLocality        : " + address.getSubLocality() +
-              "\nLocality Name      : " + address.getLocality() +
-              "\nSubAdmin Area      : " + address.getSubAdminArea() +
-              "\nAdmin Area         : " + address.getAdminArea() +
-              "\nCountryName        : " + address.getCountryName() +
-              "\nCountryCode        : " + address.getCountryCode() +
-              "\nPostal Code        : " + address.getPostalCode() +
-              "\nLocale             : " + address.getLocale() +
-              "\nLatitude           : " + address.getLatitude() +
-              "\nLongitude          : " + address.getLongitude();
-    }
-
-    @Override
-    protected void onPostExecute(String address) {
-      existingDataView.setText(address);
-    }
+    existingDataView.setText(locationDetails);
   }
+
+
+  public String getLocationDetails(Address address) {
+    return
+        "\nAddress Line       : " + address.getAddressLine(0) +
+            "\nSubLocality        : " + address.getSubLocality() +
+            "\nLocality Name      : " + address.getLocality() +
+            "\nSubAdmin Area      : " + address.getSubAdminArea() +
+//              "\nAdmin Area         : " + address.getAdminArea() +
+            "\nCountryName        : " + address.getCountryName() +
+//              "\nCountryCode        : " + address.getCountryCode() +
+//              "\nPostal Code        : " + address.getPostalCode() +
+//              "\nLocale             : " + address.getLocale() +
+            "\nLatitude           : " + address.getLatitude() +
+            "\nLongitude          : " + address.getLongitude();
+  }
+
+
 }
 
 
